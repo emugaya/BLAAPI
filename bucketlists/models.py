db72d1204fbc0bc7e2f212e47b63e9dd65649ee3
Ezekiel Mugaya <emugaya@gmail.com>
2017-06-18 22:50:09 +0300

    Added User, BucketList, and Item Models models.py

---
+++ bucketlists/models.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True)
+#     email = db.Column(db.String(120), unique=True)
+#     # password =
+#
+#     def __init__(self, username, email):
+#         self.username = username
+#         self.email = email
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+#
+# class BuckelLists(db:Model):
+#     id = db.column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), unique=True)
+#     date_created = db.Column(db.DateTime)
+#     date_modified = db.Column(db.DateTime)
+#     creted_by =
