bb91f51b26d3dddad5d9999a06dbcbe2b95c4148
Ezekiel Mugaya <emugaya@gmail.com>
2017-06-20 12:49:08 +0300

    Implementation of Creating Bucket list, Listing all buckets lists and listing single bucket list item

--- bucketlists/views.py
+++ bucketlists/views.py
@@ -1,8 +1,11 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_restful import reqparse, abort, Api, Resource
-from bucketlists import app
-import bucketlists.models
+from bucketlists import app, db
+from bucketlists.models import BucketList, User, Item
+from sqlalchemy.orm import class_mapper, session, sessionmaker
+from json import dumps
+from datetime import datetime, date
 
 api = Api(app)
 
@@ -12,21 +15,42 @@ class Helloworld(Resource):
 
 api.add_resource(Helloworld, '/v1/')
 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('buckect_list_id')
+
+def dump_datetime(value):
+    """Deserialize datetime object into string form for JSON processing."""
+    if value is None:
+        return None
+    return (value.strftime("%Y-%m-%d") +" "+ value.strftime("%H:%M:%S"))
+
 #Get list of all BucketLists and add BucketList
-class BucketLists(Resource):
+class BucketListView(Resource):
     def get(self):
-        pass
+        bucket_lists = BucketList.query.all()
+        return make_response(jsonify([i.serialize for i in bucket_lists]))
 
     def post (self):
-        pass
+        args = parser.parse_args()
+        new_bucketlist = BucketList(args.name)
+        db.session.add(new_bucketlist)
+        db.session.commit()
+        return 201
 
 # Add resource for Adding single bucket list and listing all created bucket lists
-api.add_resource(BucketLists,'/v1/bucketlists/')
+api.add_resource(BucketListView,'/v1/bucketlists/')
 
 #Display/View/Get, Delete, Update Single BucketList
-class BucketList(Resource):
+class BucketListViews(Resource):
     def get(self, buckect_list_id):
-        pass
+        args = parser.parse_args()
+        bucket_list_item = BucketList.query.filter(BucketList.id == buckect_list_id).all()
+        single_bucket_list_items = Item.query.filter(Item.bucketlist_id == buckect_list_id).all()
+        b_item = ([i.serialize for i in single_bucket_list_items])
+        x  = 1
+        return  make_response(jsonify([i.serialize_id(b_item) for i in bucket_list_item]))
+        # return  ([i.serialize for i in single_bucket_list_items])
 
     def delete(self, buckect_list_id):
         pass
@@ -35,7 +59,7 @@ class BucketList(Resource):
         pass
 
 # Add resource for getting, updating, and deleting a single bucket list
-api.add_resource(BucketList,'/v1/bucketlists/<id>')
+api.add_resource(BucketListViews,'/v1/bucketlists/<buckect_list_id>')
 
 #Create a new item in BucketLists
 class BucketListItem(Resource):
